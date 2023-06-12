5cc023d70743027f7d693eef06f268a4a0f757cf
Shvecov <markvtailor@gmail.com>
2023-06-12 19:52:31 +0300

    Image

---
+++ app/src/main/java/com/markvtls/diploma/presentation/fragments/RealityFragment.kt
@@ -0,0 +1,36 @@
+package com.markvtls.diploma.presentation.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.google.ar.sceneform.ux.ArFragment
+import com.markvtls.diploma.databinding.FragmentRealityBinding
+
+
+class RealityFragment : Fragment() {
+
+    private var _binding: FragmentRealityBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentRealityBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
