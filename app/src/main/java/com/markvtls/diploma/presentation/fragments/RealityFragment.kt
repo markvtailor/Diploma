82a4f45c0ec70e1706821c6641757dd02e8edf76
i.shvecov <i.shvecov@wallpaperscraft.com>
2023-06-13 14:13:10 +0300

    Logout

--- app/src/main/java/com/markvtls/diploma/presentation/fragments/RealityFragment.kt
+++ app/src/main/java/com/markvtls/diploma/presentation/fragments/RealityFragment.kt
@@ -1,25 +1,18 @@
+package com.markvtls.diploma.presentation.fragments
+
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.google.ar.core.Config
-import com.google.ar.core.Session
 import com.markvtls.diploma.databinding.FragmentRealityBinding
+
 
 class RealityFragment : Fragment() {
 
     private var _binding: FragmentRealityBinding? = null
     private val binding get() = _binding!!
-    lateinit var session: Session
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        val config = Config(session)
-        session = Session(requireActivity())
-        session.configure(config)
-
-    }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -31,20 +24,12 @@ class RealityFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val surface = binding.surface
-        surface.setRenderer(MainRenderer())
-        surface.requestRender()
-
+        
     }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        session.close()
-    }
 }
-
--- app/src/main/java/com/markvtls/diploma/presentation/fragments/ar/ArCore.kt
+++ app/src/main/java/com/markvtls/diploma/presentation/fragments/ar/ArCore.kt
@@ -10,20 +10,3 @@ import java.nio.ByteBuffer
 import java.util.ArrayList
 import javax.microedition.khronos.opengles.GL10
 
-class MainRenderer(private val session: Session): GLSurfaceView.Renderer {
-    override fun onSurfaceCreated(gl: GL10?, p1: javax.microedition.khronos.egl.EGLConfig?) {
-        println("Surface created")
-        //здесь нужно выполнить инициализацию контекста
-    }
-
-    override fun onSurfaceChanged(gl: GL10?, width: Int, height: Int) {
-        println("Surface changed")
-        //здесь можно обновить контекст (например, размеры сцены)
-    }
-
-    override fun onDrawFrame(gl: GL10?) {
-        println("Draw frame")
-        val frame = session.update()
-        //... логика отрисовки
-    }
-}
